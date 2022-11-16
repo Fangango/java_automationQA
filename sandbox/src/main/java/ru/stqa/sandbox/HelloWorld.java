431d8ee31f03a4626d18a8fc18f182e9c713503c
Fangango <a-grigor@mail.ru>
2022-11-16 22:27:52 +0500

    add class Point and add example

--- sandbox/src/main/java/ru/stqa/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/sandbox/HelloWorld.java
@@ -3,6 +3,16 @@ package ru.stqa.sandbox;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		
+		Point p1 = new Point(2,5);
+		Point p2 = new Point(7, 10);
+
+		double distance = Point.distance(p1, p2);
+		System.out.println(distance);
 	}
+
+	//public static double distance(Point point1, Point point2) {
+	//    double distance = Math.sqrt(Math.pow((point2.x - point1.x), 2) + Math.pow((point2.y - point1.y), 2));
+	//    return distance;
+	//}
 }
